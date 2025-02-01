c0d3f3cc7e144b16cd87aaab5198349a84334fac
ezekielswanson <zeke@aptitude8.com>
2025-01-31 21:29:30 -0600

    Updated files

---
+++ index.js
@@ -0,0 +1,13 @@
+//Selecting inputs
+
+
+const textInput = document.querySelector('#txt');  
+const rateInput = document.querySelector('#rate');  
+const pitchInput = document.querySelector('#pitch');  
+
+
+
+
+
+console.log(window)
+
