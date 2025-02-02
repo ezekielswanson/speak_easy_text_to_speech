f33137260bb9c9828ed1fa1b00e80c998134d237
ezekielswanson <zeke@aptitude8.com>
2025-02-02 16:10:55 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,9 @@
 const textInput = document.querySelector('#txt');  
 const rateInput = document.querySelector('#rate');  
 const pitchInput = document.querySelector('#pitch');  
+const voiceOptions = document.querySelector('selector')
+
+console.log(voiceOptions)
 
 
 
@@ -14,8 +17,26 @@ const speechSynth = window.speechSynthesis;
 //Voices function
 
 /*
-store voice in array
-loop through voices
+get these list of voice
+    Samantha
+    script.js:30 Aaron
+    script.js:30 Albert
+    script.js:30 Alice
+    script.js:30 Alva
+    script.js:30 Amélie
+    script.js:30 Amira
+    script.js:30 Anna
+    script.js:30 Arthur
+    script.js:30 Bad News
+    script.js:30 Bahh
+    script.js:30 Bells
+
+-store & display those voice in the seelect tag
+-voices only display when an on voiceschanged event occurs
+
+
+
+
 
 
 */
@@ -24,6 +45,8 @@ speechSynth.addEventListener("voiceschanged", () => {
 
     //On the speech synt object get voices methdo and store in var
     //how do I read docuemnation to see what methods I need, etc
+    //a method generall retunrs somehting so that's why we store in var?
+    //update this code
     const voices = speechSynth.getVoices();
     speechSynth.getVoices().forEach(voice => console.log(voice.name))
 
