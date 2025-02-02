5ec45fbd5403bc6e3d863bd415f47267739f933c
ezekielswanson <zeke@aptitude8.com>
2025-02-01 23:12:56 -0600

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,29 @@ const pitchInput = document.querySelector('#pitch');
 
 
 
-
+//Gives me access to the API
 const speechSynth = window.speechSynthesis;
 
+//Voices function
 
-speechSynth.getVoices().forEach(voice => console.log(voice.name))
+/*
+store voice in array
+loop through voices
+
+
+*/
+
+speechSynth.addEventListener("voiceschanged", () => {
+
+    //On the speech synt object get voices methdo and store in var
+    //how do I read docuemnation to see what methods I need, etc
+    const voices = speechSynth.getVoices();
+    speechSynth.getVoices().forEach(voice => console.log(voice.name))
+
+})
+
+
+
 
 
 
