34fa41c109e4edc734e4bb7cf51b826f601ea17f
ezekielswanson <zeke@aptitude8.com>
2025-02-03 21:54:51 -0600

    Update script.js

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ speechSynth.addEventListener("voiceschanged", () => {
 //could potentially pass in voices, pitch, etc, as a param here to use voice
 
 playBtn.addEventListener('click', () => {
+    console.log('btn clicked');
     const utterance = new SpeechSynthesisUtterance(textInput);
     console.log(utterance)
     speechSynth.speak(utterance);
